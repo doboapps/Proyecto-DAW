6598ff25431d6945257e9e4f0c55684857257484
Alberto Domínguez <albercala@gmail.com>
2018-03-25 18:55:05 +0200

    second commit heroku

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports={
   port: process.env.PORT || 3000,
   SECRET_TOKEN:"mi llave sercreta",  
-  dbShop:process.env.MOGODB ||'mongodb://localhost:27017/shop',// si estoy en produccion conectate a tal si no a la de modo desarrollo
+  //dbShop:process.env.MOGODB ||'mongodb://localhost:27017/shop',// si estoy en produccion conectate a tal si no a la de modo desarrollo
+  dbShop:process.env.MONGODB_URI ||'mongodb://localhost:27017/shop',// si estoy en produccion conectate a tal si no a la de modo desarrollo 
   dbSession:process.env.MOGODB ||'mongodb://localhost:27017/authsession',
   ADMIN_WEBSITE:'admin@admin.com'
 }
